2d3aa6f5119b45629c021746945c89de9bf0722a
NoyRobinson <noyrob96@gmail.com>
2024-06-30 17:37:11 +0300

    changed logger

--- backend/logger.js
+++ backend/logger.js
@@ -1,19 +1,19 @@
-morgan = require('morgan')
 const fs = require('fs');
 const path = require('path');
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' });
 
-morgan (toLog = (tokens, req, res) => {
-    return [
-        `Time of request: ${new Date()}`,
-        `HTTP method: ${tokens.method(req, res)}`,
-        `Target path: ${tokens.url(req, res)}`,
-        `Body: ${JSON.stringify(req.body, null, 2)}`,
-        '---'
+const requestLogger = (req, res, next) => {
+    const log = [
+        `time: ${new Date()}`,
+        `HTTP request method: ${req.method}`,
+        `request target path: ${req.path}`,
+        `request body: ${JSON.stringify(req.body, null, 2)}`,
+        '-------'
     ].join('\n');
-});
 
-const logger = morgan(toLog, { stream: accessLogStream });
+    accessLogStream.write(log + '\n');
+    next();
+}
 
-module.exports = logger;
+module.exports = requestLogger;
